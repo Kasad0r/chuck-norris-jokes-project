1077d958098cc03627c419b16aea9bcb0f184e5e
kasad0r <2431082dima>
2020-07-14 19:41:51 +0200

    Replace  by XML Configuration Chuck Beans

--- src/main/java/kasad0r/chucknorrisjokes/ChuckNorrisJokesApplication.java
+++ src/main/java/kasad0r/chucknorrisjokes/ChuckNorrisJokesApplication.java
@@ -2,12 +2,14 @@ package kasad0r.chucknorrisjokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChuckNorrisJokesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChuckNorrisJokesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisJokesApplication.class, args);
+    }
 
 }
